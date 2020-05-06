157c5ee7cf571e26f5c4d95b38a76436a3382719
Jordan2551 <Jordan1558@gmail.com>
2020-05-05 23:39:09 -0400

    fixed splash page for abrowsers

--- assets/my_splash.js
+++ assets/my_splash.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
-//   	$('header').remove();
+    $('header').remove();
     $('footer').remove();
 
     let leafContainer = $(".leaf-container");
 
     $("#retailer").hover(function (e) { 
-        leafContainer.animate({opacity:"0.7", speed: 300});
-      	leafContainer.animate({opacity:"1", speed: 100});
+      	// leafContainer.animate({opacity:"1", speed: 100});
         leafContainer.css("background-image", "url('https://cdn.shopify.com/s/files/1/0098/7482/t/8/assets/leaves-left-hover.png')");
+        // leafContainer.animate({opacity:"0.7"});
+        // leafContainer.animate({opacity:"1"});
     });
     $("#personal").hover(function (e) { 
-        leafContainer.animate({opacity:"0.7", speed: 300});
-        leafContainer.animate({opacity:"1", speed: 100});
         leafContainer.css("background-image", "url('https://cdn.shopify.com/s/files/1/0098/7482/t/8/assets/leaves-right-hover.png')");
+        // leafContainer.animate({opacity:"0.7"});
+        // leafContainer.animate({opacity:"1"});
     });
 
     const mouseLeave = () => {
